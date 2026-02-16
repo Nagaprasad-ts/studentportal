dbf99561405b269fea9f924902b8b8dfe32d3a00
nagaprasad-ts <tsnagaprasadts@gmail.com>
2026-02-16 11:27:50 +0530

    small fix

--- resources/js/pages/Admin/ImportStudents.tsx
+++ resources/js/pages/Admin/ImportStudents.tsx
@@ -1,4 +1,3 @@
-import type { Method } from '@inertiajs/core';
 import { Form, Head } from '@inertiajs/react';
 import InputError from '@/components/input-error';
 import { Button } from '@/components/ui/button';
